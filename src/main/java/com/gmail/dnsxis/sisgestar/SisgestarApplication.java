c155ec883288b1978f5cb95c03923bd031b1c3b6
Denis Xismendes <dnsxis@gmail.com>
2021-09-28 14:23:34 -0300

    arquivos iniciais de projeto

---
+++ src/main/java/com/gmail/dnsxis/sisgestar/SisgestarApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.dnsxis.sisgestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgestarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/dnsxis/sisgestar/SisgestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.dnsxis.sisgestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
